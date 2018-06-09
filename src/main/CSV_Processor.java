def97c922e1eb2eb57afb61cdcf1397db7fc1d9c
Juan <koldur.c@gmail.com>
2018-06-09 21:13:39 +0200

    Main and View documented

--- src/main/CSV_Processor.java
+++ src/main/CSV_Processor.java
@@ -2,12 +2,19 @@ package main;
 
 import view.FileChooser;
 
+/**
+ * @author Juan Gomez-Martinho
+ *
+ */
 public class CSV_Processor {
 
 	
-	
+	/**
+	 * Main method for the app. It launches the view and makes it visible.
+	 * @param args default main args.
+	 */
 	public static void main(String[] args) {
-		FileChooser c = new FileChooser();
-		c.setVisible(true);
+		FileChooser c = new FileChooser(); //Create window
+		c.setVisible(true); //Make it visible
 	}
 }
--- src/view/FileChooser.java
+++ src/view/FileChooser.java
@@ -20,6 +20,15 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 import business.Processor;
 
+/**
+ * View for the app. It manages the file chooser and executes the processor.
+ * @author Juan Gomez-Martinho
+ *
+ */
+/**
+ * @author juan
+ *
+ */
 public class FileChooser extends JFrame {
 
 	/**
@@ -27,24 +36,30 @@ public class FileChooser extends JFrame {
 	 */
 	private static final long serialVersionUID = 7558101181907117923L;
 	
-	private JFileChooser fc;
-	private JPanel body;
-	private HashMap<Integer, File> files;
-	private HashMap<Integer, JLabel> filenames;
-	private HashMap<Integer, JButton> fileselect;
-	private JButton addField, about, go, exit;
-	private int nextID;
+	private JFileChooser fc; // Only one file chooser, so the last directory is saved between file selctions.
+	private JPanel body; //One unique body for the view.
 	
+	// Index --> File, Name, Button
+	private HashMap<Integer, File> files; //HashMap that stores the pair Index-File
+	private HashMap<Integer, JLabel> filenames; //HashMap to store the pair Index-File Name.
+	private HashMap<Integer, JButton> fileselect; //HashMap to store the pair Index-Button to re-select the file.
+	
+	private JButton addField, about, go, exit; // Single buttons to control the program
+	private int nextID; // Variable to store the next index to use with the HashMaps
+	
+	/**
+	 *  Default and only constructor. No arguments needed.
+	 */
 	public FileChooser() {
-		super();
-		loadWindow();
-		loadContent();
+		super(); // Call the super constructor from JFrame, from which it extends.
+		loadWindow(); // Load the main window (JFrame attributes)
+		loadContent(); // Initializes it's own attributes
 	}
 	
 	private void loadWindow() {
-		setTitle("CSV standardizer");
-		//setSize(500,200);
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setTitle("CSV standardizer"); // Window title
+		//setSize(500,200); //Sets the window size
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE); //When 
 		
 		/*
 		 * As a design choice, the window will be displayed in the middle of the screen.
@@ -64,109 +79,116 @@ public class FileChooser extends JFrame {
 		
 		files = new HashMap<Integer, File>(); //File list initializer
 		filenames = new HashMap<Integer, JLabel>(); //Label list initializer
-		fileselect = new HashMap<Integer, JButton>();
+		fileselect = new HashMap<Integer, JButton>(); //Button list initializer
 		
-		addField = new JButton("Add more files...");
-		addField.addActionListener(new ActionListener() {
+		addField = new JButton("Add more files..."); //The button to add files have a self-explanatory text
+		addField.addActionListener(new ActionListener() { // Add an action to the button when pressed
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				addFileField();
+				addFileField(); //When pressed, ad a field to the current view
 			}
 		});
 		
-		about = new JButton("About");
-		about.addActionListener(new ActionListener() {
+		about = new JButton("About"); //The button to see the program instructions has a self-explanatory text 
+		about.addActionListener(new ActionListener() { //Add an action to the button when pressed
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				showAbout(getTitle());
+				showAbout(getTitle()); //When pressed, show the "about" window
 			}
 		});
 		
-		nextID = 0;
+		nextID = 0; //The id count starts on 0
 		
-		go = new JButton("Process"); //Process the files
-		go.addActionListener(new ActionListener() {
+		go = new JButton("Process"); //The button to Process the files has a self-explanatory text
+		go.addActionListener(new ActionListener() { //Add an action to the button when pressed
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				execute();
+				execute(); //When pressed, call to the method that starts the processing
 			}
 		});
 		
-		exit = new JButton("Exit");
-		exit.addActionListener(new ActionListener() {
+		exit = new JButton("Exit"); //The button to exit the program has a self-explanatory text
+		exit.addActionListener(new ActionListener() { //Ad an action to the button when pressed
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
+				System.exit(0); //Exit the current program.
 			}
 		});
 		
 		//Panel initializer. The value will be replaced when fileField is added though addFileField
 		body = new JPanel(new GridLayout(3,3));
-		add(body);
+		add(body); //Finally, add the JPanel to the current JFrame
 		
-		addFileField();
+		addFileField(); //ad the file fields to the window
 	}
 	
 
+	/**
+	 *  Method to add a new field to choose a new File.
+	 */
 	private void addFileField() {
-		remove(body);
+		remove(body); //To repaint the view, remove the current one.
 		
-		final int currentID = nextID;
+		final int currentID = nextID; //The current ID to be worked with is the current value of nextId
 		
-		File e = null;
-		files.put(currentID, e);
+		File e = null; //Create a new file object, null at the beginning 
+		files.put(currentID, e); //Put the file in the HashMap with the saved id.
 		
-		JLabel lab = new JLabel("(File not selected yet)");
-		filenames.put(currentID, lab);
+		JLabel lab = new JLabel("(File not selected yet)"); //The label to show the file name. By default, it has none.
+		filenames.put(currentID, lab); //Put the label in the HashMap with the saved id.
 	
-		JButton sel = new JButton("Explore...");
-		sel.addActionListener(new ActionListener() {
+		JButton sel = new JButton("Explore..."); //A new button to select the file
+		sel.addActionListener(new ActionListener() { //Execute when the button is pressed.
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//In response to a button click:
-				int returnVal = fc.showOpenDialog(body);
-				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					files.put(currentID, fc.getSelectedFile());
-					filenames.get(currentID).setText(fc.getSelectedFile().getName());
-		        } else {
-		           System.out.println("Open command cancelled by user.");
+				int returnVal = fc.showOpenDialog(body); //Open the file chooser, and store the result
+				if (returnVal == JFileChooser.APPROVE_OPTION) { // If the return value from the chooser is correct
+					files.put(currentID, fc.getSelectedFile()); //Put the result file into the HashMap with the unique id.
+					filenames.get(currentID).setText(fc.getSelectedFile().getName()); //Set the label text to show the file name.
+		        } else { //File not chosen
+		           //Do nothing
 		        }
 			}
 		});
-		fileselect.put(currentID, sel);
+		fileselect.put(currentID, sel); //Put the created button into the HashMap with it's id.
 		
-		nextID++;
+		nextID++; //Having the components added, increase the next id.
 		
-		body = new JPanel(new GridLayout( (fileselect.size()+2) , 3) );
+		body = new JPanel(new GridLayout( (fileselect.size()+2) , 3) ); //The new panel has a row for each file field plus 2 (add file, go+exit)
 		
-		for(int i = 0; i < fileselect.size(); i++) {
+		for(int i = 0; i < fileselect.size(); i++) { //Create the row: Label + File Name + Button
 			JLabel current = new JLabel("File " + i + ":");
 			body.add(current);
 			body.add(filenames.get(i));
 			body.add(fileselect.get(i));
 		}
-		body.add(addField);
-		body.add(new JLabel());
-		body.add(about);
-		body.add(go);
-		body.add(new JLabel());
-		body.add(exit);
+		body.add(addField); //Add the button to expand the fields
+		body.add(new JLabel()); //Add a blank space
+		body.add(about); //Add the about button
+		body.add(go); //Add the execute button
+		body.add(new JLabel()); //Add a blank space
+		body.add(exit); //Add the exit button
 		
 
-		setSize(500,((fileselect.size()+2)*36));
-		add(body);
+		setSize(500,((fileselect.size()+2)*36)); //Set the new dimensions depending on the amount of current fields
+		add(body); //finally, add the body
 	}
 	
+	/**
+	 * Method to show the about window
+	 * @param title String containing the window title.
+	 */
 	private void showAbout(String title) {
-		JFrame about = new JFrame(title);
-		about.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		JTextPane area = new JTextPane();
+		JFrame about = new JFrame(title); //New window / frame
+		about.setDefaultCloseOperation(DISPOSE_ON_CLOSE); //When the window is closed, dispose that window only.
 		
-		area.setEditable(false);
+		JTextPane area = new JTextPane(); //Text pane to contain the body
+		area.setEditable(false); //The text will not be editable
 		
-		final String newline = System.lineSeparator();
-		
+		final String newline = System.lineSeparator(); //Get the system line separator
+		//The text containing the about description:
 		String content = "CSV Standarizer" + 
 					newline 
 					+ newline 
@@ -187,23 +209,26 @@ public class FileChooser extends JFrame {
 					+ "The program will generate a csv with the attributes:"
 					+ newline
 					+ "AUTHOR,CONTENT,CLASS";
-		area.setText(content);
-		JScrollPane pane = new JScrollPane(area);
-		about.add(pane);
+		area.setText(content); //Set the text into the Text Area
+		JScrollPane pane = new JScrollPane(area); //Create a scrollable pane containing the Text Area
+		about.add(pane); //Add the the pane into the window
 		
-		about.setSize(320,350);
-		about.setResizable(false);
-		about.setVisible(true);
+		about.setSize(320,350); //Set default size
+		about.setResizable(false); //Not resizable
+		about.setVisible(true); //Set visible
 	}
 	
+	/**
+	 * Method that executes the file's processing 
+	 */
 	private void execute() {
-		if(!files.isEmpty()) {
-			Processor controller = new Processor();
+		if(!files.isEmpty()) { //Not execute if there's no files to process
+			Processor controller = new Processor(); //Create the Application Controller
 			try {
-				File file = controller.compute(files);
-				if(file!=null) JOptionPane.showMessageDialog(this, "File saved:\n" + file.getAbsolutePath());
+				File file = controller.compute(files); // try to compute the files
+				if(file!=null) JOptionPane.showMessageDialog(this, "File saved:\n" + file.getAbsolutePath()); //If the return isn't null, show the path of the result
 			} catch (Exception e) {
-				//Show error
+				//Show error.
 				e.printStackTrace();
 			}
 		} else {
